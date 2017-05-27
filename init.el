96555197464682349dcf0ca97c8a2579a6226d8a
chemisiertyran <christian.meyer68@gmail.com>
2017-05-27 09:37:37 +0200

    Changed modifiers keys for OSX.

--- init.el
+++ init.el
@@ -11,3 +11,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq mouse-wheel-scroll-amount '(0.01))
+;; set keys for Apple keyboard, for emacs in OS X
+(setq mac-command-modifier 'meta) ; make cmd key do Meta
+(setq mac-option-modifier 'super) ; make opt key do Super
+(setq mac-control-modifier 'control) ; make Control key do Control
+(setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
